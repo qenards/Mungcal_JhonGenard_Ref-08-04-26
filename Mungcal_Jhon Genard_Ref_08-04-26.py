895e249f49fb02eea8ecddaeae8edda7b7f0522c
qenards <jmgenardblack@gmail.com>
2026-08-05 20:57:38 +0800

    Add files via upload

---
+++ Mungcal_Jhon Genard_Ref_08-04-26.py
@@ -0,0 +1,7 @@
+number=int(input("Enter a number:"))
+print(float(5))
+name="Alice"
+print(name)
+print(type(name))
+Alice=True
+print(Alice, type (Alice))
---
+++ student.py
@@ -0,0 +1,8 @@
+count = 1
+
+while True:
+    print("Alice")
+    count = count + 1
+
+    if count > 5:
+        break
